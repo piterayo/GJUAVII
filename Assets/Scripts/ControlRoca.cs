bf791791a03fce7fab244699fb95fbec7e4783ee
Carlos Sanchis Server <Carlos Sanchis Server>
2016-07-16 18:22:58 +0200

    Personaje muy avanzado, altar en progreso

---
+++ Assets/Scripts/ControlRoca.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ControlRoca : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void disableAll()
+	{
+		GetComponent<BoxCollider2D> ().enabled = false;
+		GetComponent<SpriteRenderer> ().enabled = false;
+	}
+
+	public void enableAll()
+	{
+		GetComponent<BoxCollider2D> ().enabled = true;
+		GetComponent<SpriteRenderer> ().enabled = true;
+	}
+
+}
---
+++ Assets/Scripts/Control_Movimiento.cs
@@ -0,0 +1,89 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Control_Movimiento : MonoBehaviour {
+
+	public float maxSpeed = 10f;
+	public float AlturaSalto = 10f;
+	public int powerUp = 0; // 0 NO HAY - 1 ROCA - ETC
+	bool facingRight = true;
+	Animator anim;
+
+	bool grounded = false;
+
+	Rigidbody2D rigidBody;
+
+	// Use this for initialization
+	void Start () {
+		rigidBody = GetComponent<Rigidbody2D> ();
+		anim = GetComponent<Animator> ();
+	}
+
+	void Update() {
+		float move = Input.GetAxis("MovimientoHorizontal"+transform.name.ToString());
+		anim.SetFloat ("Speed", Mathf.Abs (move));
+
+		if (GetComponentInChildren<colisionSuelo> ().isGrounded ()) {
+			grounded = true;
+
+		} else {
+			grounded = false;
+		}
+
+		if(grounded && Input.GetButton("Salto"+transform.name.ToString()))
+			rigidBody.velocity = new Vector2 (rigidBody.velocity.x, AlturaSalto);
+
+		switch (powerUp) {
+		case 0: // No lleva power up
+			GetComponentInChildren<ControlRoca>().disableAll();
+
+			break;
+		case 1: // Lleva DA ROCK
+			GetComponentInChildren<ControlRoca>().enableAll();
+
+			break;
+
+		}
+
+
+	}
+
+	void FixedUpdate () {
+
+		//float move = Input.GetAxis ("Horizontal");
+
+		float move = Input.GetAxis("MovimientoHorizontal"+transform.name.ToString());
+
+		anim.SetFloat ("Speed", Mathf.Abs (move));
+
+		rigidBody.velocity = new Vector2 (move * maxSpeed, rigidBody.velocity.y);
+	
+		if (move > 0 && !facingRight)
+			Flip ();
+		else if(move <0 && facingRight)
+			Flip();
+	
+	}
+
+
+	void Flip()
+	{
+		facingRight = !facingRight;
+		Vector3 theScale = transform.localScale;
+		theScale.x *= -1;
+		transform.localScale = theScale;
+	}
+
+	void OnCollisionEnter2D(Collision2D Colision)
+	{
+		print (Colision.transform.name);
+
+
+		if (Colision.transform.name == "rock")
+			powerUp = 1;
+
+
+	}
+
+
+}
